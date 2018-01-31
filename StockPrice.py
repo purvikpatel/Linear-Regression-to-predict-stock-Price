d7a40631975b77fe4e6b16fd47226ddde9284c64
Purvik Patel <purvikpatel619@gmail.com>
2018-01-31 23:16:34 +0530

    Add files via upload

---
+++ StockPrice.py
@@ -0,0 +1,36 @@
+import csv
+import numpy as np
+from statistics import mean
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+
+
+dates = []
+X = [[],[],[],[],[]]
+y = []
+
+
+def get_data(filename):
+    with open(filename, 'r') as csvfile:
+        csvFileReader = csv.reader(csvfile)
+        next(csvFileReader)  # skipping column names
+        for row in csvFileReader:
+            dates.append(int(row[0].split('-')[0]))
+            X[0].append(float(row[1]))
+            X[1].append(float(row[2]))
+            X[2].append(float(row[3]))
+            X[3].append(float(row[5]))
+            y.append(float(row[4]))
+    return
+
+
+get_data('aapl.csv')
+
+model = linear_model.LinearRegression()
+model.fit(X,y)
+
+Y = model.predict(X)
+
+plt.scatter(dates,y ,  color='black')
+plt.plot(dates, Y, color='blue', linewidth=3)
+plt.show()
